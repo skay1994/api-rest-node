b0d57d97a8fdad19d84a43845a8e6557069b907b
Jorge Carlos <skay_1994@yahoo.com.br>
2021-09-18 10:28:19 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+require('dotenv').config()
+
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./../swagger.json');
+
+const routers = require('./routes');
+
+app.use(routers)
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(process.env.APP_PORT, () => {
+    console.log(`The application is listening on port ${process.env.APP_PORT}!`);
+})
