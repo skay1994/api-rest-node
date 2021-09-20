f46c6e1f4a1b8d887dc7eb88239119b600ac58d2
Jorge Carlos <skay_1994@yahoo.com.br>
2021-09-20 16:47:07 -0300

    Create account.controller.ts

---
+++ src/controllers/account.controller.ts
@@ -0,0 +1,41 @@
+import { Request, Response, Router, NextFunction } from 'express'
+import { celebrate } from 'celebrate'
+
+import Controller from './base.controller'
+import accountModel from '../models/account.model'
+
+import AccountListSchema from '../formSchemas/accounts/list.schema'
+import AccountAddAmountSchema from '../formSchemas/accounts/add_amount.schema'
+
+class AccountController extends Controller {
+    public routers(): Router {
+        this.router.post('/', celebrate(AccountListSchema), this.get)
+        this.router.post('/add', celebrate(AccountAddAmountSchema), this.addAmount)
+
+        return this.router
+    }
+
+    private get(request: Request, response: Response) {
+        /*  #swagger.tags = ['User']
+        #swagger.description = 'List user gy id' */
+        accountModel.find({ user_id: request.body.user_id }, (err: any, account: any) => {
+            if (account) {
+                response.send({ success: true, account: account });
+            } else {
+                response.status(404).send({ success: false, error: 'Account not found' })
+            }
+        })
+    }
+
+    private addAmount(request: Request, response: Response) {
+        accountModel.updateOne({ user_id: request.body.user_id }, { $inc: { current_balance: request.body.amount } }, (err: any) => {
+            if (err) {
+                response.status(404).send({ success: false, error: 'Error on add value to balance' });
+            }
+
+            response.send({ success: true, message: "Amount added with successful to accont balance" });
+        })
+    }
+}
+
+export default AccountController
