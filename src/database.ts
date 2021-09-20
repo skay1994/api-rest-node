3325066e9041552d4f105228fc5e6f9ecd0ea871
Jorge Carlos <skay_1994@yahoo.com.br>
2021-09-20 13:40:37 -0300

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -5,6 +5,6 @@ const {
     MONGO_PASS,
     MONGO_PATH,
 } = process.env;
-
+mongoose.Promise = global.Promise;
 mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_PATH}`)
     .catch(error => console.log(error));
