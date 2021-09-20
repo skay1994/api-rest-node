8475a30626a3550de2e7e4fff9013e7a2c29b03b
Jorge Carlos <skay_1994@yahoo.com.br>
2021-09-20 16:47:38 -0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express';
 import UserController from './../controllers/user.controller'
+import AccountController from './../controllers/account.controller'
 
 const router = require('express').Router()
 
 router.use('/api/users', new UserController().routers());
+router.use('/api/accounts', new AccountController().routers());
 
 module.exports = router
